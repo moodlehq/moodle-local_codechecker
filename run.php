150ef8137ec375a4804491a81a2776671cf705de
Tim Hunt <T.J.Hunt@open.ac.uk>
2012-02-15 16:28:38 +0000

    Codechecker changes made for OU CI server.

--- run.php
+++ run.php
@@ -26,12 +26,19 @@
 define('CLI_SCRIPT', true);
 
 require(dirname(__FILE__) . '/../../config.php');
+
+raise_memory_limit(MEMORY_HUGE);
+set_time_limit(3600);
+
 require_once($CFG->libdir . '/clilib.php');
 require_once($CFG->dirroot . '/local/codechecker/locallib.php');
 
 
 // Get the command-line options.
-list($options, $unrecognized) = cli_get_params(array('help' => false), array('h' => 'help'));
+list($options, $unrecognized) = cli_get_params(
+    array('help' => false, 'type' => false, 'loglvl' => false, 'file' => false),
+    array('h' => 'help', 't' => 'type', 'l' => 'loglvl', 'f' => 'file')
+);
 
 if (count($unrecognized) != 1) {
     $options['help'] = true;
@@ -44,17 +51,69 @@ if ($options['help']) {
     die();
 }
 
-raise_memory_limit(MEMORY_HUGE);
+$verboseness = 0;
+if ($options['type'] == 'xml') {
+    $type = 'Xml';
+} else if ($options['type'] == 'checkstyle') {
+    $type = 'Checkstyle';
+} else {
+    $type = 'full';
+    $verboseness = 1;
+}
+
+// Override the default verboseness if specified
+// Note: This will cause the script to output logging data so if you want to save the xml you will
+// have to specify the xmlfile param
+// 1: Print progress information.
+// 2: Print developer debug information.
+if ($options['loglvl']) {
+    switch($options['loglvl']) {
+        case 1:
+        case 2:
+            $verboseness = $options['loglvl'];
+            break;
+        default:
+            $verboseness = 0;
+            break;
+    }
+}
 
 $standard = $CFG->dirroot . str_replace('/', DIRECTORY_SEPARATOR, '/local/codechecker/moodle');
 
-$phpcs = new PHP_CodeSniffer(1);
+$phpcs = new PHP_CodeSniffer($verboseness);
 $phpcs->setCli(new local_codechecker_codesniffer_cli());
 $phpcs->setIgnorePatterns(local_codesniffer_get_ignores());
 $numerrors = $phpcs->process(local_codechecker_clean_path(
         $CFG->dirroot . '/' . trim($path, '/')),
         local_codechecker_clean_path($standard));
 
-$reporting       = new PHP_CodeSniffer_Reporting();
+$reporting = new PHP_CodeSniffer_Reporting();
 $problems = $phpcs->getFilesErrors();
-$reporting->printReport('full', $problems, false, null);
+
+if ($options['file'] == 'auth/manual.xml') {
+    require_once('nofile.php');
+}
+
+if ($options['file']) {
+    //  Start output buffering
+    ob_start();
+}
+
+$reporting->printReport($type, $problems, false, null);
+
+if ($options['file']) {
+    //  End output buffering
+    $data = ob_get_clean();
+
+    $file = $options['file'];
+    $filepath = $CFG->dataroot.'/codechecker/'.$file;
+
+    //  Make sure the codechecker folder exists
+    make_upload_directory('codechecker');
+
+    file_put_contents($filepath, $data);
+
+    if ($verboseness > 0) {
+        echo "File [MOODLEDATA]/codechecker/$file saved\n";
+    }
+}
