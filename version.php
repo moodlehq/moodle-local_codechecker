f98216f782a99a4049e52e72cee660f2324079e0
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2012-04-03 11:39:01 +0200

    NOBUG: Fixed copy/paste typo.

--- version.php
+++ version.php
@@ -26,4 +26,4 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2012030100;
 $plugin->requires  = 2011120500; // Moodle 2.2 release and upwards
-$module->component = 'local_codechecker';
+$plugin->component = 'local_codechecker';
