1b5d471620af4c39b62f6be58579d198de52fb74
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2013-02-14 01:50:39 +0100

    CONTRIB-4144 Bump to 2013021400 (v2.2.5)
    Changes:
    - CONTRIB-4151: added moodle phpunit support (via local_codechecker_testcase).
    - CONTRIB-4149: added phpcompatibility sniffs (git://github.com/wimg/PHPCompatibility.git).
    - CONTRIB-4145: upgrade to PHPCS 1.4.4.
    - CONTRIB-4144: add (this) CHANGES.txt file.

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012061600;
-$plugin->release   = '2.2.2';
+$plugin->version   = 2013021400;
+$plugin->release   = '2.2.5';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2011120500; // Moodle 2.2 release and upwards
 $plugin->component = 'local_codechecker';
