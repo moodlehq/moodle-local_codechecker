d016bac2f8a074b8824798b2836b5c46216e9ba5
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2013-09-01 16:43:13 +0200

    Add missing MOODLE_INTERNAL to fixtures.

--- moodle/tests/fixtures/squiz_whitespace_operatorspacing.php
+++ moodle/tests/fixtures/squiz_whitespace_operatorspacing.php
@@ -1,4 +1,6 @@
 <?php
+defined('MOODLE_INTERNAL') || die(); // Make this always the 1st line in all CS fixtures.
+
 // Array representations.
 $array = array('test' => 'Test');
 $array = array('test'=> 'Test');
