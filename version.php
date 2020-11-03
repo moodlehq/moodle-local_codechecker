afc4d971cd3c9d47f5f44dd233b738b7e32c7d98
Víctor Déniz <victor@moodle.com>
2020-11-03 08:04:49 +0000

    Identify the release as beta
    Required changes in version.php to identify the release as a beta.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2020110200;
-$plugin->release   = '3.0.0';
-$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '3.0.0beta';
+$plugin->maturity  = MATURITY_BETA;
 $plugin->requires  = 2017111300; // Moodle 3.4 release and upwards.
 $plugin->component = 'local_codechecker';
