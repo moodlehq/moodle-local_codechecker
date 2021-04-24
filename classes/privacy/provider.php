ec50c3b169287e53a65a2695bdbea82f0aacf411
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2021-04-24 11:21:52 +0200

    Remove old (33_STABLE) legacy_polyfill use
    As far as this plugin only supports 34_STABLE and up
    the polyfill can now be safely removed.
    
    Also, note the it violates the PSR2/PSR12 method naming
    which implies that not underscore _ prefixed methods
    should be used.

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -34,16 +34,13 @@ defined('MOODLE_INTERNAL') || die();
  */
 class provider implements \core_privacy\local\metadata\null_provider {
 
-    // To provide php 5.6 (33_STABLE) and up support.
-    use \core_privacy\local\legacy_polyfill;
-
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
      *
      * @return  string
      */
-    public static function _get_reason() {
+    public static function get_reason(): string {
         return 'privacy:metadata';
     }
 }
